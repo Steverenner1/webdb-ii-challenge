420105cee2aef1071a2a59f8ca9995a068c16908
Steve Renner <che1111@hotmail.com>
2019-11-05 13:38:01 -0800

    Added migration and server/index

--- migrations/20191105130444_add_cars.js
+++ migrations/20191105130444_add_cars.js
@@ -1,8 +1,17 @@
 
 exports.up = function(knex) {
-  
+  return knex.schema.createTable("cars", function(table) {
+    table.increments();
+
+    table.string('VIN').notNullable().unique();
+    table.string('make', 128).notNullable();
+    table.string('model', 128).notNullable();
+    table.integer('mileage').notNullable();
+    table.string('transmission_type');
+    table.integer('title_status');
+  });
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTableIfExists('cars');
 };
